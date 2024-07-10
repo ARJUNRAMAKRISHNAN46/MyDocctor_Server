eac85fd517498eb946ef7e070281952783ae659f
ARJUN P <arjunpchr@gmail.com>
2024-07-10 13:58:25 +0530

    refactor: updated path issue

--- User/src/application/interfaces/IDependencies.ts
+++ User/src/application/interfaces/IDependencies.ts
@@ -1,7 +1,28 @@
+import {
+  IAddServiceUseCase,
+  IAddSpecialityUseCase,
+  IFindUserByEmailUseCase,
+  IFindUserByIdUseCase,
+  IListServiceUseCase,
+  IListSpecialityUseCase,
+  IListUserUseCase,
+  IUpdateUserProfileUseCase,
+  IVerifyUserUseCase,
+} from "@/domain/useCaseInterface";
 import { IRepositories } from "./IRepositories";
-import { IUseCases } from "./../interfaces/IUseCase";
+// import { IUseCases } from "./IUseCase";
 
 export interface IDependencies {
   repositories: IRepositories;
-  useCases: IUseCases;
+  useCases: {
+    findUserByEmailUseCase: (dependencies: any) => IFindUserByEmailUseCase;
+    findUserByIdUseCase: (dependencies: any) => IFindUserByIdUseCase;
+    verifyUserUseCase: (dependencies: any) => IVerifyUserUseCase;
+    listUserUseCase: (dependencies: any) => IListUserUseCase;
+    addSpecialityUseCase: (dependencies: any) => IAddSpecialityUseCase;
+    listSpecialityUseCase: (dependencies: any) => IListSpecialityUseCase;
+    updateProfileUseCase: (dependencies: any) => IUpdateUserProfileUseCase;
+    addServiceUseCase: (dependencies: any) => IAddServiceUseCase;
+    listServiceUseCase: (dependencies: any) => IListServiceUseCase;
+  };
 }
