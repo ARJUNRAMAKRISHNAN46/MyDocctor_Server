a8834a862023622eb3f0620e279ec0a29ee44301
ARJUN P <arjunpchr@gmail.com>
2024-07-10 18:10:33 +0530

    feat: kafka port added

--- User/src/infrastructure/kafka/index.ts
+++ User/src/infrastructure/kafka/index.ts
@@ -2,7 +2,8 @@ import { Kafka, Producer, Consumer } from "kafkajs";
 
 export const kafka = new Kafka({
     clientId: "user-service-latest",
-    brokers: ["localhost:29092"]
+    // brokers: ["localhost:29092"]
+    brokers: ["apache-kafka-service:29092"]
 })
 
 export const producer: Producer = kafka.producer();
