68b84a601bcd32af1dbdf7c79ce8e0a70c2045f5
ARJUN P <arjunpchr@gmail.com>
2024-07-10 16:14:39 +0530

    feat: small console added for preview

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,17 +1,18 @@
-import server from './presentation/server';
-import dbConnection from './infrastructure/database/dbConnection';
-import { runConsumer } from './infrastructure/kafka/consumer';
+import server from "./presentation/server";
+import dbConnection from "./infrastructure/database/dbConnection";
+import { runConsumer } from "./infrastructure/kafka/consumer";
 
-(async() => {
-    try {
-        server;
-        await dbConnection()
-        .catch((error: any) => {
-            console.log(error?.message);
-            process.exit();
-        });
-        await runConsumer().catch(console.error);
-    } catch (error: any) {
-        console.log(error?.message);
-    }
+(async () => {
+  try {
+    console.log("🌿🌿🌿🌿🌿🌿🌿  Entry Point   🔰🔰🔰🔰🔰🔰🔰");
+
+    server;
+    await dbConnection().catch((error: any) => {
+      console.log(error?.message);
+      process.exit();
+    });
+    await runConsumer().catch(console.error);
+  } catch (error: any) {
+    console.log(error?.message);
+  }
 })();
