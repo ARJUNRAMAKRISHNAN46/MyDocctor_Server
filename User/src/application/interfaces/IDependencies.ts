accf9ce979a0c7d5e9de1fbbe7453a837e084592
ARJUN P <arjunpchr@gmail.com>
2024-07-10 14:01:37 +0530

    refactor: test commend added

--- User/src/application/interfaces/IDependencies.ts
+++ User/src/application/interfaces/IDependencies.ts
@@ -1,28 +1,29 @@
-import {
-  IAddServiceUseCase,
-  IAddSpecialityUseCase,
-  IFindUserByEmailUseCase,
-  IFindUserByIdUseCase,
-  IListServiceUseCase,
-  IListSpecialityUseCase,
-  IListUserUseCase,
-  IUpdateUserProfileUseCase,
-  IVerifyUserUseCase,
-} from "@/domain/useCaseInterface";
+// import {
+//   IAddServiceUseCase,
+//   IAddSpecialityUseCase,
+//   IFindUserByEmailUseCase,
+//   IFindUserByIdUseCase,
+//   IListServiceUseCase,
+//   IListSpecialityUseCase,
+//   IListUserUseCase,
+//   IUpdateUserProfileUseCase,
+//   IVerifyUserUseCase,
+// } from "@/domain/useCaseInterface";
 import { IRepositories } from "./IRepositories";
-// import { IUseCases } from "./IUseCase";
+import { IUseCases } from "../../application/interfaces/IUseCase";
 
 export interface IDependencies {
   repositories: IRepositories;
-  useCases: {
-    findUserByEmailUseCase: (dependencies: any) => IFindUserByEmailUseCase;
-    findUserByIdUseCase: (dependencies: any) => IFindUserByIdUseCase;
-    verifyUserUseCase: (dependencies: any) => IVerifyUserUseCase;
-    listUserUseCase: (dependencies: any) => IListUserUseCase;
-    addSpecialityUseCase: (dependencies: any) => IAddSpecialityUseCase;
-    listSpecialityUseCase: (dependencies: any) => IListSpecialityUseCase;
-    updateProfileUseCase: (dependencies: any) => IUpdateUserProfileUseCase;
-    addServiceUseCase: (dependencies: any) => IAddServiceUseCase;
-    listServiceUseCase: (dependencies: any) => IListServiceUseCase;
-  };
+  useCases: IUseCases;
+  //  {
+  //   findUserByEmailUseCase: (dependencies: any) => IFindUserByEmailUseCase;
+  //   findUserByIdUseCase: (dependencies: any) => IFindUserByIdUseCase;
+  //   verifyUserUseCase: (dependencies: any) => IVerifyUserUseCase;
+  //   listUserUseCase: (dependencies: any) => IListUserUseCase;
+  //   addSpecialityUseCase: (dependencies: any) => IAddSpecialityUseCase;
+  //   listSpecialityUseCase: (dependencies: any) => IListSpecialityUseCase;
+  //   updateProfileUseCase: (dependencies: any) => IUpdateUserProfileUseCase;
+  //   addServiceUseCase: (dependencies: any) => IAddServiceUseCase;
+  //   listServiceUseCase: (dependencies: any) => IListServiceUseCase;
+  // };
 }
