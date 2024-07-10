0d6e252bc0e59d14c2e6dc673639e24ed2e22e28
ARJUN P <arjunpchr@gmail.com>
2024-07-10 15:48:06 +0530

    test: workflow

--- notification/src/mail/mail.service.ts
+++ notification/src/mail/mail.service.ts
@@ -18,7 +18,7 @@ export class MailService {
   }
 
   async sendMail(email: string, message: string) {
-    console.log('🚀 ~ MailService ~ sendMail ~ email: ', email, message);
+    console.log('🚀 ~ MailService ~ sendMail ~ email:', email, message);
     const mailOptions = {
       from: 'mydocctor@gmail.com',
       to: email,
