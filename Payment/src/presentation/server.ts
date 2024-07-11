23640a3234938b089fcab091ef0b3048112b897d
ARJUN P <arjunpchr@gmail.com>
2024-07-11 18:12:13 +0530

    refactor: remove all localhost from code

--- Payment/src/presentation/server.ts
+++ Payment/src/presentation/server.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://mydocctor.online');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mydocctor.online');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
