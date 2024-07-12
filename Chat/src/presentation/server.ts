5f39a75bd1094e901f9f361a4663157819d5e9af
ARJUN P <arjunpchr@gmail.com>
2024-07-12 14:33:05 +0530

    refactor: replace all frontend urls with new one

--- Chat/src/presentation/server.ts
+++ Chat/src/presentation/server.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 const server = http.createServer(app)
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mydocctor.online');
+  res.setHeader('Access-Control-Allow-Origin', 'https://my-docctor.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
