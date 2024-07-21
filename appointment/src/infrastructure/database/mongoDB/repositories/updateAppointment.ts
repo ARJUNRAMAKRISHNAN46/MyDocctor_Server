71eebca18de013336b49dcff9fb0d530cecc7778
ARJUN P <arjunpchr@gmail.com>
2024-07-22 00:39:26 +0530

    refactor: appointment updation edited and added the paymentId to it

--- appointment/src/infrastructure/database/mongoDB/repositories/updateAppointment.ts
+++ appointment/src/infrastructure/database/mongoDB/repositories/updateAppointment.ts
@@ -1,3 +1,37 @@
+// import { Appointment } from "../models";
+// import { AppointmentEntity } from "@/domain/entities";
+
+// export const updateAppoinment = async (
+//   data: any
+// ): Promise<AppointmentEntity | null> => {
+//   try {
+//     const updateSlot = await Appointment.findOneAndUpdate(
+//       {
+//         doctorId: data?.doctor_id,
+//         date: data?.date,
+//         "slots.start": data?.slot,
+//       },
+//       {
+//         $set: { "slots.$.userId": String(data?.user_id) },
+//       },
+//       {
+//         new: true,
+//       }
+//     );
+
+//     if (!updateSlot) {
+//       return null;
+//     }
+
+//     return updateSlot;
+//   } catch (error: any) {
+//     throw new Error(error?.message);
+//   }
+// };
+
+
+
+
 import { Appointment } from "../models";
 import { AppointmentEntity } from "@/domain/entities";
 
@@ -12,7 +46,10 @@ export const updateAppoinment = async (
         "slots.start": data?.slot,
       },
       {
-        $set: { "slots.$.userId": String(data?.user_id) },
+        $set: {
+          "slots.$.userId": String(data?.user_id),
+          "slots.$.paymentId": String(data?.paymentId)
+        },
       },
       {
         new: true,
