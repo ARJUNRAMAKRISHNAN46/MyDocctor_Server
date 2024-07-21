2f50fbef46389d0ce78ccd1c411586d4bac877fa
ARJUN P <arjunpchr@gmail.com>
2024-07-21 19:50:50 +0530

    refactor: payment controller modified

--- Payment/src/presentation/controllers/savePayments.ts
+++ Payment/src/presentation/controllers/savePayments.ts
@@ -11,9 +11,18 @@ export const savePaymentController = (dependencies: IDependencies) => {
       const data = req.body;
       console.log("🚀 ~ file: savePayment.ts:11 ~ return ~ data:", data);
       const response = await savePaymentUseCase(dependencies).execute(data);
-      if (response) {
-        console.log(response);
+      if (!response) {
+        res.status(400).json({
+          success: false,
+          message: "Payment creation failed",
+        });
+        return;
       }
+      res.status(200).json({
+        success: true,
+        message: "Payment created successfully",
+        data: response,
+      });
     } catch (error) {
       next(error);
     }
