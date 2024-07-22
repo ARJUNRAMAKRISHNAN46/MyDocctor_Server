41105454f203a86fb345207dde87dc28704ceb31
ARJUN P <arjunpchr@gmail.com>
2024-07-22 09:41:34 +0530

    test: payment service bugs

--- appointment/src/presentation/server.ts
+++ appointment/src/presentation/server.ts
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://mydocctor.online');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mydocctor.online');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
